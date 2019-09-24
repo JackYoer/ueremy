7cac25c7741be0e5cb8d7263387d1e884e3f1c8a
JackYoer <jackyoerofficial@gmail.com>
2019-09-24 19:01:05 +0200

    Base Architecture

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+import { AppRoutingModule } from './app-routing.module';
+
+import { BrowserComponent } from './browser/browser.component';
+import { EditorComponent } from './editor/editor.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { WelcomePageComponent } from './welcome-page/welcome-page.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+   imports: [
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule
+  ],
+  declarations: [ AppComponent, BrowserComponent, EditorComponent, PageNotFoundComponent, WelcomePageComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
